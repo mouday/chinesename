89bd9e6c9c3467379006e8d8cf9a7d482ac5ae23
mouday <1940607002@qq.com>
2017-11-29 13:59:07 +0800

    fix docstring

--- chinesename.py
+++ chinesename.py
@@ -1,39 +1,55 @@
 import random
 
-class ChineseName():
+class ChineseName(object):
 
     def __init__(self):
         pass
 
-    # 获取中文字符列表
-    # parameters:
-    #   filename - 文件路径:string (空格分隔)
-    # return:
-    #   list - 字符列表
     def getChineseCharacters(self, filename):
+        """获取中文字符列表
+        Args:
+            filename: 文件路径 - String (空格分隔文件)
+        Returns:
+            List: 字符列表
+        """
         with open(filename, "r") as f:
             ChineseCharacters = f.read().split(" ")
             return ChineseCharacters
-
-    # 获取姓氏
+  
     def getLastName(self):
+        """获取姓氏
+        Args:
+            None
+        Returns:
+            String: 姓氏
+        """
         lastnames = self.getChineseCharacters("百家姓全文.txt")
         lastname = random.choice(lastnames)
         return lastname
 
-    # 获取名字
     def getFirstName(self, char_count=1):
+        """获取名字
+        Args:
+            char_count: 名字长度，默认1 - Integer
+        Returns:
+            String: 名字
+        """
         firstnames = self.getChineseCharacters("姓名中常用的2897个汉字.txt")
         firstname = []
         for i in range(char_count):
             firstname.append(random.choice(firstnames))
 
         firstname = "".join(firstname)
-        # print(firstname)
         return firstname
 
-    # 获取一个中文名字
     def getChineseName(self, char_count=1, lastname=""):
+        """获取一个中文名字
+        Args:
+            char_count: 名字长度，默认1 - Integer
+            lastname: 姓氏，默认随机 - String
+        Returns:
+            String: 名字
+        """
         ChineseName = []
         if lastname == "":
             ChineseName.append(self.getLastName())
@@ -44,19 +60,25 @@ class ChineseName():
         ChineseName = "".join(ChineseName)
         return ChineseName
 
-    # 获取一个中文名字列表
     def getChineseNames(self, count, char_count=1, lastname=""):
+        """获取一个中文名字列表
+        Args:
+            count: 名字数量 - Integer
+            char_count: 名字长度，默认1 - Integer
+            lastname： 姓氏，默认随机 - String
+        Returns:
+            List: 名字列表
+        """
         ChineseNames = []
         for i in range(count):
             ChineseNames.append(self.getChineseName(char_count,lastname))
         return ChineseNames
 
-
 def main():
     chinesename = ChineseName()
     name = chinesename.getChineseName(lastname="白")
     print(name)
-    names=chinesename.getChineseNames(100)
+    names=chinesename.getChineseNames(100,char_count=2,lastname="彭")
     print(names)
 
 
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-import chinesename
+import chinesename  # 导入包
 
-cnname=chinesename.ChineseName()
-name=cnname.getChineseName()
+cnname=chinesename.ChineseName()  # 实例化
+
+name=cnname.getChineseName()  # 获取一个中文名
 print("name:\n",name)
 
-names=cnname.getChineseNames(100)
+names=cnname.getChineseNames(100)  # 获取一个中文名列表
 print("names:\n",names)
+
+print(cnname.getChineseCharacters.__doc__)  # 测试__doc__
