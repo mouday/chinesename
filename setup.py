1fdd75f85009df6b7b949c66cfdce01c644dbbd4
彭世瑜 <psy068@qimingpian.com>
2018-06-21 10:39:49 +0800

    修复pypi描述显示问题

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 import setuptools
+import os
+import requests
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# 将markdown格式转换为rst格式
+def md_to_rst(from_file, to_file):
+    r = requests.post(url='http://c.docverter.com/convert',
+                      data={'to':'rst','from':'markdown'},
+                      files={'input_files[]':open(from_file,'rb')})
+    if r.ok:
+        with open(to_file, "wb") as f:
+            f.write(r.content)
+
+
+md_to_rst("README.md", "README.rst")
+
+long_description = 'Add a fallback short description here'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setuptools.setup(
     name="chinesename",
-    version="0.0.7",
+    version="0.0.8",
     author="Peng Shiyu",
     author_email="pengshiyuyx@gmail.com",
     description="get a chinesename by random",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/mouday/chinesename",
     packages=setuptools.find_packages(),
     classifiers=(
@@ -18,8 +33,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires = [],       # 常用
     package_data = {
             # If any package contains *.txt or *.rst files, include them:
-            '': ['source/*.txt', "source/*.json"],
+            'source': ['*.txt', "*.json"],
     }
 )
